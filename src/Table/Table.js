5cdccc046687105c874c9e3b51789405335ed59e
anvadla <109344614+anvadla@users.noreply.github.com>
2022-07-15 16:48:32 +0530

    Add files via upload

---
+++ src/Table/Table.js
@@ -0,0 +1,70 @@
+// import React from 'react'
+
+// const Table=()=> {
+
+
+//     // const[cons,setCons]= useState(false);
+//     // const average=() =>{
+//     //     average =0;
+//     //     dataEelements.map((element) => {
+//     //         const{ total}=element;
+//     //         average+= total;
+//     //     })
+//     //     average/dataEelements.length;
+//     //     setCons(true);
+//     //     setAverage(average);
+
+//     const avg =
+//     item.age.reduce((sum, curr) => sum + Number(curr), 0) /
+//     item.age.length;
+
+
+
+
+//         }
+    
+
+
+//     return (
+//         <div>
+//             <table>
+//                 <thead>
+//                     <tr>
+//                         <th>Name</th>
+//                         <th>Id</th>
+//                         <th>Age</th>
+//                         <th>Total</th>
+//                     </tr>
+
+//                 </thead>
+//                 <tbody>
+                  
+//                     <tr>
+//                         <td>Ajay</td>
+//                         <td>1</td>
+//                         <td>24</td>
+//                     </tr>
+//                     <tr>
+//                         <td>Syed</td>
+//                         <td>2</td>
+//                         <td>22</td>
+
+//                     </tr>
+//                     <tr>
+//                         <td>Deepan</td>
+//                         <td>3</td>
+//                         <td>22</td>
+
+//                     </tr>
+
+//                 </tbody>
+
+//             </table>
+
+//             <button onClick ={()=> average()}>AVERAGE</button>
+//         </div>
+//     )
+
+
+
+// export default Table;
